50f2284711b68e411095b59048fed279718fc4de
ysys720 <74398207+ysys720@users.noreply.github.com>
2026-04-15 19:56:47 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120805. 몫 구하기/몫 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        if (0<num1 && num1<=100 && 0<num2 && num2<=100) {
+            return num1 / num2;
+        }
+        return -1;
+    }
+}
